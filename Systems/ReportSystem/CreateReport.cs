1f5259737d5639634869f766d309d11e79d4bd42
KAYDEN <102880179+0xKAYDEN@users.noreply.github.com>
2025-01-25 21:59:22 +0300

    report system v1.0.0.0

---
+++ Systems/ReportSystem/CreateReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeadRabbitProject.ReportSystem
+{
+    public class CreateReport
+    {
+        static string ReportID()
+        {
+            string repoID = "";
+            return repoID;
+        }
+        public static void SubmitReport(uint UID, string From ,string playerName, string Details)
+        {
+
+        }
+    }
+}
---
+++ Systems/ReportSystem/UpdateReportsRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadRabbitProject.ReportSystem
+{
+    internal class UpdateReportsRecord
+    {
+        public static void ReportsCount()
+        {
+
+        }
+        public static void UpdateRecord(uint UID, string PlayerName, int ReportsCount)
+        {
+
+        }
+    }
+}
